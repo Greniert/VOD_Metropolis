2a7f101bc4f7764d9d1e6d12ccde4c4e4220eae3
Greniert <39262221+Greniert@users.noreply.github.com>
2018-07-12 10:03:56 +0200

    Add files via upload

---
+++ Include/header.php
@@ -0,0 +1,30 @@
+<header>
+  		<div class=" image-header" id="imagelogo" >
+        <img src="img/logo.jpg" height="200px">
+  			<img src="img/ban.jpg" height="200px" width="">
+  		</div>
+
+
+  		<nav class="navbar navbar-default" id="navbar" data-spy="affix" data-offset-top="200">
+  			<div class="container-fluid">
+  				<div class="navbar-header">
+  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menubar">
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  				</button>
+  				</div>
+
+  				<div class="collapse navbar-collapse" id="menubar">
+  			 	<ul class="nav navbar-nav">
+  					<li class="active"><a href="#accueil">Accueil</a></li>
+  					<li><a href="#apropos">Louer un film</a></li>
+  				</ul>
+          <ul class="nav navbar-nav connex" >
+            <li><a href="#galerie">S'inscire</a></li>
+            <li><a href="#contact">Connexion</a></li>
+          </ul>
+  				</div>
+  			</div>
+  		</nav>
+</header>
